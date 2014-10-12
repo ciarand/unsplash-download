6a328a9501c81d62df18c160551c2e60269d6862
Ciaran Downey <me@ciarand.me>
2014-10-12 01:43:00 -0700

    Use more concurrent stuff + ease of life goodies
    General quality of life improvements, including:
    
     - Retry downloads up to 3 times
    
     - Configurable (hardcoded) number of gothreads running at any point
    
     - Doesn't download an image you've already got
    
     - Better error messages all around

--- unsplash.go
+++ unsplash.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 type Image struct {
@@ -18,49 +21,115 @@ type Image struct {
 	Post_Url   string
 }
 
-func Download(image Image) {
+func Download(image Image) error {
+	// if we've already got it, don't re-get it
+	if _, err := os.Stat("images/" + image.Filename); err == nil {
+		return nil
+	}
 
 	res, err := http.Get(image.Post_Url + "/download")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
-	defer res.Body.Close()
 
 	err = ioutil.WriteFile("images/"+image.Filename, body, 0664)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	log.Println("Downloading " + images.Filename)
+	fmt.Printf("done downloading %s\n", image.Filename)
+	return nil
 }
 
 func main() {
+	// increase the timeout
+	http.DefaultClient.Timeout = time.Second * 20
+
 	// GET all of our JSON
 	res, err := http.Get("https://unsplash.it/list")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Print("Couldn't retrieve unsplash.it list:", err)
+		os.Exit(1)
 	}
+	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Print("Couldn't read the unsplash.it list:", err)
+		os.Exit(1)
 	}
-
-	defer res.Body.Close()
 
 	var images []Image
-	err = json.Unmarshal(body, &images)
-	if err != nil {
-		log.Fatal(err)
+	if err = json.Unmarshal(body, &images); err != nil {
+		fmt.Printf("Couldn't unmarshal json (%s): %s", string(body), err)
+		os.Exit(1)
 	}
 
-	for _, img := range images {
-		Download(img)
+	if len(images) == 0 {
+		fmt.Printf("images collection is empty: %s", string(body))
+		os.Exit(1)
 	}
+
+	max := 3
+	imgWg := &sync.WaitGroup{}
+	imgWg.Add(len(images))
+
+	threadWg := &sync.WaitGroup{}
+	threadWg.Add(max)
+
+	conns := make(chan Image, max)
+
+	// begin the "producer" loop
+	go func() {
+		for _, v := range images {
+			conns <- v
+		}
+	}()
+
+	// begin the consumer loop w/ a total of 10 gothreads
+	for i := 0; i < max; i += 1 {
+		go func(num int) {
+			// each of them will loop and block on receive
+			for {
+				select {
+				case img := <-conns:
+					// and try 3 times to download a file
+					for tries := 0; tries < 3; tries += 1 {
+						if err := Download(img); tries == 2 && err != nil {
+							fmt.Printf("Couldn't download %s: %s\n", img.Filename, err)
+						} else if err == nil {
+							// successful get
+							break
+						}
+					}
+
+					imgWg.Done()
+
+				case <-time.After(1 * time.Minute):
+					fmt.Printf("worker thread #%d done\n", num)
+					threadWg.Done()
+					return
+				}
+			}
+		}(i)
+	}
+
+	done := make(chan bool)
+
+	go func() {
+		threadWg.Wait()
+		done <- true
+	}()
+	go func() {
+		imgWg.Wait()
+		done <- true
+	}()
+
+	<-done
 }
